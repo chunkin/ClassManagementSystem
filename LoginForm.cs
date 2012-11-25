86ba0f05b9304b3246fb3fcf04fd0aeef0b23bf9
chunkin <wu0211@gmail.com>
2012-11-25 19:21:30 +0800

    Login Form

---
+++ LoginForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClassManagementSystem
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var username = this.username.Text;
+            var password = this.password.Text;
+            if (username == "admin" && password == "admin")
+            {
+                MessageBox.Show("login successfully!");
+            }
+        }
+    }
+}
