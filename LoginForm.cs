90893d541002f0ef322cbdabe69ad3eff0b06a59
chunkin <wu0211@gmail.com>
2012-11-25 22:49:07 +0800

    add MainForm and access db support

--- LoginForm.cs
+++ LoginForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,10 +21,30 @@ namespace ClassManagementSystem
         {
             var username = this.username.Text;
             var password = this.password.Text;
-            if (username == "admin" && password == "admin")
+            if (IsAdmin(username, password))
             {
-                MessageBox.Show("login successfully!");
+                var mainForm = new MainForm();
+                Hide();
+                mainForm.Show();
             }
+        }
+
+        private bool IsAdmin(string username, string password)
+        {
+            var connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\chunkin\Documents\Visual Studio 2010\Projects\ClassManagementSystem\cmsdb\cmsdb.accdb;Persist Security Info=False;";
+            var connection = new OleDbConnection(connectionString);
+            connection.Open();
+            string query = String.Format("select count(*) from users where username='{0}' and password='{1}'", username, password);
+            var command = new OleDbCommand(query, connection);
+            OleDbDataReader reader = command.ExecuteReader();
+            int count = 0;
+            while (reader.Read())
+            {
+                count = reader.GetInt32(0);
+            }
+            reader.Close();
+            connection.Close();
+            return count == 1;
         }
     }
 }
---
+++ MainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ClassManagementSystem
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.usersManagementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.orgnizationManagementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deparrtmentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.classToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usersManagementToolStripMenuItem,
+            this.orgnizationManagementToolStripMenuItem});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(837, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // usersManagementToolStripMenuItem
+            // 
+            this.usersManagementToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem});
+            this.usersManagementToolStripMenuItem.Name = "usersManagementToolStripMenuItem";
+            this.usersManagementToolStripMenuItem.Size = new System.Drawing.Size(121, 20);
+            this.usersManagementToolStripMenuItem.Text = "Users Management";
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.addToolStripMenuItem.Text = "Add";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.addUserMenuItem_Click);
+            // 
+            // orgnizationManagementToolStripMenuItem
+            // 
+            this.orgnizationManagementToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deparrtmentToolStripMenuItem,
+            this.classToolStripMenuItem});
+            this.orgnizationManagementToolStripMenuItem.Name = "orgnizationManagementToolStripMenuItem";
+            this.orgnizationManagementToolStripMenuItem.Size = new System.Drawing.Size(155, 20);
+            this.orgnizationManagementToolStripMenuItem.Text = "Orgnization Management";
+            // 
+            // deparrtmentToolStripMenuItem
+            // 
+            this.deparrtmentToolStripMenuItem.Name = "deparrtmentToolStripMenuItem";
+            this.deparrtmentToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.deparrtmentToolStripMenuItem.Text = "Deparrtment";
+            // 
+            // classToolStripMenuItem
+            // 
+            this.classToolStripMenuItem.Name = "classToolStripMenuItem";
+            this.classToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.classToolStripMenuItem.Text = "Class";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(837, 584);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem usersManagementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem orgnizationManagementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deparrtmentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem classToolStripMenuItem;
+    }
+}
